20161aebb44a22c2e281f3fd31b0f608b8cbd710
pr0kuk <shcherbakov.aa@phystech.edu>
2022-10-01 18:17:29 +0300

    restructured class Point

--- point.hpp
+++ point.hpp
@@ -1,16 +1,19 @@
 #pragma once
 #include "libs.hpp"
-#include "pixel.hpp"
-//#include "vorcel.hpp"
 class Point
 {
     public:
+        int color;
         int x = -1;
         int y = -1;
-        std::vector<Pixel> refpixels;
-        //Vorcell cell;
         std::pair<int, int> get_cord();
         int set_cord(int , int);
         Point();
         Point(int, int);
+};
+
+class CenterPoint: public Point
+{
+        //std::vector<Pixel> refpixels;
+        //Vorcell cell;
 };
