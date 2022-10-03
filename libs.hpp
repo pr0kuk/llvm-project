749ea183c9a3675955e122857760b7a13148dc0e
pr0kuk <alex-shch@yandex.ru>
2022-10-03 19:48:23 +0300

    extracted not_used dir

--- libs.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#define HEIGHT 500
-#define WIDTH 500
-#define NUMBER_OF_POINTS 10
-#define COLOR_OF_POINT -1
-#define MAX_INT 10e8
-#define FRAME_TIME 40
-#include <iostream>
-#include <vector>
--- point.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "libs.hpp"
-class Point
-{
-    public:
-        int color = 0;
-        int x = 0;
-        int y = 0;
-};
