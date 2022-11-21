e3f6db8303fdede43c463a16831878ff47e7da49
pr0kuk <alex-shch@yandex.ru>
2022-11-22 02:46:31 +0300

    added tests

--- glang/11.sh
+++ glang/11.sh
@@ -2,5 +2,3 @@ lex l.lex
 bison -d grammar.y
 cmake ./
 make
-printf "int i = 0;\nint y[20] = 10;\nint pow(int a){\nreturn a*a;\n}\nint main(){\n  i = 2;\n  for i from 0 to 11 {y[i] = pow(i);\n}\n  y[1] = 1;\nreturn 0;\n}"
-printf "int i = 0;\nint y[20] = 10;\nint pow(int a){\nreturn a*a;\n}\nint main(){\n  i = 2;\n  for i from 0 to 11 {y[i] = pow(i);\n}\n  y[1] = 1;\nreturn 0;\n}" | ./pars
